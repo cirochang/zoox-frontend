37be2e77007d5769a3a110277240a733404f3366
Ciro Chang <ciro.chang@tvglobo.com.br>
2018-08-27 00:39:45 -0300

    add token in request headers

--- config/env-dev.js
+++ config/env-dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"development"',
   BACKEND_API_URI: JSON.stringify("http://localhost:3000/api/v1"),
-  MOCK_REQUESTS: 'false'
+  BACKEND_API_KEY: '"foobar"'
 };
--- config/env-prod.js
+++ config/env-prod.js
@@ -1,4 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  BACKEND_API_URI: JSON.stringify("http://localhost:3000/api/v1")
+  BACKEND_API_URI: JSON.stringify("http://localhost:3000/api/v1"),
+  BACKEND_API_KEY: '"foobar"'
 };
